2ae17425513da922b2145ee5cc2c538445f7a8cb
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2025-10-08 22:41:50 +0600

    controlled element each

--- src/Components/ControlledField/ControlledField.jsx
+++ src/Components/ControlledField/ControlledField.jsx
@@ -4,11 +4,23 @@ const ControlledField = () => {
     const handleSubmit=(e)=>{
         e.preventDefault();
        //  console.log('submitted');
+       if(password.length<6){
+        setError('password should be 6 char');
+       }else{
+        setError('');
+       }
     }
     const handleOnchange=(e)=>{
           console.log(e.target.value);
+          setPassword(e.target.value);
+        //   if(password.length<6){
+        //     setError('PassWord Should Be 6 Character');
+        //   }else{
+        //     setError('');
+        //   }
     }
     const[password,setPassword]=useState('');
+    const [error,setError]=useState('');
     return (
         <div>
             <form onSubmit={handleSubmit}>
@@ -19,6 +31,7 @@ const ControlledField = () => {
               <input type="submit" value="Submit" />
 
             </form>
+            <p style={{color:'red'}}><small>{error}</small></p>
         </div>
     );
 };
