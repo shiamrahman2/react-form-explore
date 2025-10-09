6c9dba9f57f048e03a27fbbdaa0c0ca0a114c574
shiamrahman2 <mohammodshiambhuiyan@gmail.com>
2025-10-09 20:23:05 +0600

    useContext for Global Use

--- src/Components/FamilyTree/FamilyTree.jsx
+++ src/Components/FamilyTree/FamilyTree.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { createContext, useState } from 'react';
 import Gandpa from './Gandpa';
 import './family.css';
 
+ export const moneyContext=createContext(0);
 const FamilyTree = () => {
     const asset='Diamond';
+    const [money,setMoney]=useState(0);
     return (
         <div className='family-tree'>
             <h2>Family Tree</h2>
              <h3>Family Asset:Diamond</h3>
-            <Gandpa asset={asset}></Gandpa>
+             <h1>Family Balance :{money}</h1>
+           <moneyContext.Provider value={[money,setMoney]}>
+             <Gandpa asset={asset}></Gandpa>
+           </moneyContext.Provider>
         </div>
     );
 };
--- src/Components/FamilyTree/Special.jsx
+++ src/Components/FamilyTree/Special.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { use } from 'react';
+import { moneyContext } from './FamilyTree';
+import { useContext } from 'react';
 
 const Special = ({asset}) => {
+     const [money, setMoney] = useContext(moneyContext);
+
+    const handleAddMoney = () =>{
+        setMoney(money + 5000);
+    }
     return (
         <div>
             <h2>Special:{asset}</h2>
+            <button onClick={handleAddMoney}>Add Balance 5k to Family Balance</button>
         </div>
     );
 };
